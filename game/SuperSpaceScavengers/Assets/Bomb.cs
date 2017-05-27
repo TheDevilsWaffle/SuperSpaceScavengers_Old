6c658b8a6264d13b1020e82658c2651bd9915662
TheDevilsWaffle <TheDevilsWaffle@gmail.com>
2017-05-26 17:25:18 -0700

    added a simple bomb to blow up players/enemies/walls.

---
+++ game/SuperSpaceScavengers/Assets/Bomb.cs
@@ -0,0 +1,209 @@
+﻿﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+//AUTHOR — Travis Moore
+//SCRIPT — Bomb.cs
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#pragma warning disable 0169
+#pragma warning disable 0649
+#pragma warning disable 0108
+#pragma warning disable 0414
+
+using UnityEngine;
+//using UnityEngine.UI;
+using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine.Rendering;
+
+#region ENUMS
+//public enum EnumStatus
+//{
+//	
+//};
+#endregion
+
+#region EVENTS
+//public class EVENT_EXAMPLE : GameEvent
+//{
+//    public EVENT_EXAMPLE() { }
+//}
+#endregion
+
+public class Bomb : MonoBehaviour
+{
+    #region FIELDS
+    [Header("EXPLOSION")]
+    [SerializeField]
+    bool armed;
+    bool isExploding = false;
+    [SerializeField]
+    float power = 1f;
+    [SerializeField]
+    float radius = 5f;
+    [SerializeField]
+    float upForce = 10f;
+
+    [Header("TIMER")]
+    [SerializeField]
+    float timer;
+
+    Transform tr;
+    GameObject bomb;
+    [SerializeField]
+    Color exploding;
+    #endregion
+
+    #region INITIALIZATION
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// OnValidate
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void OnValidate()
+    {
+        //get refs
+        tr = GetComponent<Transform>();
+        bomb = tr.GetChild(0).gameObject;
+        //set initial values
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Awake
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Awake()
+    {
+        //SetSubscriptions();
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Start
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Start()
+    {
+    
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// SetSubscriptions
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void SetSubscriptions()
+    {
+        //Events.instance.AddListener<>();
+    }
+    #endregion
+
+    #region UPDATE
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Update()
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Update()
+    {
+        if(armed)
+        {
+            if(!isExploding)
+            {
+                isExploding = !isExploding;
+                LeanTween.color(bomb, exploding, 0.25f).setLoopType(LeanTweenType.pingPong);
+            }
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f)
+            {
+                Explode();
+            }
+        }
+        
+
+    #if false
+        UpdateTesting();
+    #endif
+    }
+    #endregion
+
+    #region PUBLIC METHODS
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+
+    #endregion
+
+    #region PRIVATE METHODS
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void Explode()
+    {
+        Vector3 _pos = tr.position;
+        Collider[] _colliders = Physics.OverlapSphere(_pos, radius);
+        
+        foreach(Collider _hit in _colliders)
+        {
+            Rigidbody _rb = _hit.GetComponent<Rigidbody>();
+            if(_rb != null)
+            {
+                _rb.AddExplosionForce(power, _pos, radius, upForce, ForceMode.Impulse);
+            }
+        }
+        Destroy(this.gameObject);
+    }
+    #endregion
+
+    #region ONDESTORY
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// OnDestroy
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void OnDestroy()
+    {
+        //remove listeners
+        //Events.instance.RemoveListener<>();
+    }
+    #endregion
+
+    #region TESTING
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// UpdateTesting
+    /// </summary>
+    ///////////////////////////////////////////////////////////////////////////////////////////////
+    void UpdateTesting()
+    {
+        //Keypad 0
+        if(Input.GetKeyDown(KeyCode.Keypad0))
+        {
+
+        }
+        //Keypad 1
+        if(Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            
+        }
+        //Keypad 2
+        if(Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            
+        }
+        //Keypad 3
+        if(Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            
+        }
+        //Keypad 4
+        if(Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            
+        }
+        //Keypad 5
+        if(Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            
+        }
+        //Keypad 6
+        if(Input.GetKeyDown(KeyCode.Keypad6))
+        {
+            
+        }
+    }
+    #endregion
+}
